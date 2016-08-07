c347963f50c217535f0546fd2c1365f70726b7c6
jhamer02 <jhamer02@hotmail.com>
2016-08-07 12:00:20 -0500

    Initial Commit

---
+++ Spotify Ex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spotify Ex
+//
+//  Created by Jorge Jhamil Pineda Echeverria on 07/08/16.
+//  Copyright © 2016 jjpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Spotify Ex/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Spotify Ex
+//
+//  Created by Jorge Jhamil Pineda Echeverria on 07/08/16.
+//  Copyright © 2016 jjpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
